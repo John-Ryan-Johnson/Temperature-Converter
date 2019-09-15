ce0294828ad35aee8d016c88a1ad64f793172b91
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-09-15 14:47:47 -0500

    final commit

--- main.js
+++ main.js
@@ -1,27 +1,70 @@
-const printToDom = (divId, textToPrint) => {
-  const selectedDiv = document.getElementById(divId);
-  selectedDiv.innerHTML = textToPrint;
-};
+const celsiusRadioButton = document.getElementById('celRadio');
+const fahrenheitRadioButton = document.getElementById('fahRadio');
+const temp = document.getElementById('number');
 
-/*
-const toCelsius =  () => {
-
+const printToDOM = (divID, textToPrint) => {
+    document.getElementById(divID).innerHTML = textToPrint;
 }
 
-const toFahrenheit =  () => {
+const toCelsius =  (temp) => {
+    const newTemp = Math.round((temp - 32) * 5 / 9);
+    if (newTemp >= 32) {
+        document.getElementById('output').style.color = 'red';
+    } else if (newTemp <= 0 ) {
+        document.getElementById('output').style.color = 'blue';
+    } else if (newTemp > 0 && newTemp < 32) {
+        document.getElementById('output').style.color = 'green';
+    }
+    printToDOM('output', `<h3>${newTemp}</h3>`);
+}
 
+const toFahrenheit =  (temp) => {
+    const newTemp = Math.round(temp * 9 / 5 + 32);
+    if (newTemp >= 90) {
+        document.getElementById('output').style.color = 'red';
+    } else if (newTemp <= 32 ) {
+        document.getElementById('output').style.color = 'blue';
+    } else if (newTemp > 32 && newTemp < 90) {
+        document.getElementById('output').style.color = 'green';
+    }
+    printToDOM('output', `<h2>${newTemp}</h2>`);
 }
 
 // Get a reference to the button element in the DOM
 const button = document.getElementById("converter");
+const clearButton = document.getElementById('reset');
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 const determineConverter = (e) => {
-  console.log("event", e);
+  const buttonID = e.target.id;
+  if (buttonID === 'converter' && fahrenheitRadioButton.checked === true) {
+    toFahrenheit(temp.value);
+  } else if (buttonID === 'converter' && celsiusRadioButton.checked === true) {
+      toCelsius(temp.value);
+  } 
+}
+
+const enterFunction = (e) => { 
+    const key = e.keyCode; 
+    if (key === 13 && fahrenheitRadioButton.checked === true) {
+        e.preventDefault();
+        toFahrenheit(temp.value);
+    } else if (key === 13 && celsiusRadioButton.checked === true) {
+        e.preventDefault();
+        toCelsius(temp.value);
+    }
+}
+
+const clearClick = (e) => {
+    const clearButtonID = e.target.id; 
+    if (clearButtonID === 'reset') {
+        temp.value = '';
+        printToDOM('output', temp.value);
+    }
 }
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
-*/
-
+temp.addEventListener('keypress', enterFunction);
+clearButton.addEventListener('click', clearClick);
